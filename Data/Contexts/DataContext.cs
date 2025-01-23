b66f90b426b42ae4b61b62d610a801e98d4faf52
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-01-23 08:47:02 +0100

    Add project files.

---
+++ Data/Contexts/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<CustomerEntity> entities { get; set; }
+}
---
+++ Data/Entities/CustomerEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class CustomerEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string LastName { get; set; } = null!; 
+
+    [Required]
+    [Column(TypeName = "varchar(150)")]
+    public string Email { get; set; } = null!;
+    
+    public string PhoneNumber { get; set; } = null!;
+
+}
